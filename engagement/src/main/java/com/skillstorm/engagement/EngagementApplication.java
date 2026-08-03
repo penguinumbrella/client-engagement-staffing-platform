d837433bf79ee406aa6e63073c1c70f80de389bb
Gina Peng <ginapeng2002@gmail.com>
2026-08-03 11:11:01 -0500

    feat: added remaining projects, eveyrthing connected to api gateway

---
+++ engagement/src/main/java/com/skillstorm/engagement/EngagementApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.engagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngagementApplication.class, args);
+	}
+
+}
---
+++ engagement/src/test/java/com/skillstorm/engagement/EngagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.engagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
