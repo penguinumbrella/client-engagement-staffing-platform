aab675e69aa689bef85fcdaf85a7fcea026a6654
Gina Peng <ginapeng2002@gmail.com>
2026-08-18 13:35:40 -0500

    merging fixes

--- staffing/src/main/java/com/skillstorm/staffing/Config/RestClientConfig.java
+++ staffing/src/main/java/com/skillstorm/staffing/Config/RestClientConfig.java
@@ -1,4 +1,4 @@
-package com.skillstorm.staffing.Config;
+package com.skillstorm.staffing.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
