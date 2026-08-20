1400a2976e78b57c4670ed7891ad9bfd12eecc55
Gina Peng <ginapeng2002@gmail.com>
2026-08-19 22:26:51 -0500

    uuid ag

--- staffing/src/main/java/com/skillstorm/staffing/repository/ConsultantRepository.java
+++ staffing/src/main/java/com/skillstorm/staffing/repository/ConsultantRepository.java
@@ -2,6 +2,8 @@ package com.skillstorm.staffing.repository;
 
 import com.skillstorm.staffing.model.Consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +14,9 @@ public interface ConsultantRepository
 
     List<Consultant> findByActiveTrue();
 
-    Optional<Consultant> findByUserId(UUID userId);
+    @Query(value = "SELECT * FROM consultants WHERE user_id = CAST(:userId AS uuid)", nativeQuery = true)
+    Optional<Consultant> findByUserId(@Param("userId") UUID userId);
 
-    Optional<Consultant> findByUserIdAndActiveTrue(UUID userId);
+    @Query(value = "SELECT * FROM consultants WHERE user_id = CAST(:userId AS uuid) AND is_active = true", nativeQuery = true)
+    Optional<Consultant> findByUserIdAndActiveTrue(@Param("userId") UUID userId);
 }
