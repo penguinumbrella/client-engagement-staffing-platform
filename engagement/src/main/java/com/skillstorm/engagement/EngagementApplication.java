5100cc7b44eb0cfb58cfc86f20aaeab0b0d36c8e
Gina Peng <ginapeng2002@gmail.com>
2026-08-24 16:22:03 -0500

    fix tests

--- engagement/src/main/java/com/skillstorm/engagement/EngagementApplication.java
+++ engagement/src/main/java/com/skillstorm/engagement/EngagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EngagementApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(EngagementApplication.class, args);
 	}
 
