758533f490b8eda59b0e8435ccad4eb7a5e2868f
Gina Peng <ginapeng2002@gmail.com>
2026-08-31 10:03:10 -0500

    staffing

--- staffing/src/main/java/com/skillstorm/staffing/StaffingApplication.java
+++ staffing/src/main/java/com/skillstorm/staffing/StaffingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StaffingApplication {
+public class StaffingApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(StaffingApplication.class, args);     
