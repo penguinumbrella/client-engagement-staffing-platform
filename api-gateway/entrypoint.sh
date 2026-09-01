235dbbff895eb16fac5207f1f135b681af982175
Gina Peng <ginapeng2002@gmail.com>
2026-08-31 20:55:57 -0500

    test

--- api-gateway/entrypoint.sh
+++ api-gateway/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+echo "entrypoint: ECS_CONTAINER_METADATA_URI_V4=$ECS_CONTAINER_METADATA_URI_V4"
+
 if [ -n "$ECS_CONTAINER_METADATA_URI_V4" ]; then
-    TASK_IP=$(curl -s "$ECS_CONTAINER_METADATA_URI_V4" | jq -r '.Networks[0].IPv4Addresses[0]')
+    METADATA=$(curl -s "$ECS_CONTAINER_METADATA_URI_V4")
+    echo "entrypoint: metadata response: $METADATA"
+    TASK_IP=$(echo "$METADATA" | jq -r '.Networks[0].IPv4Addresses[0]')
+    echo "entrypoint: resolved TASK_IP=$TASK_IP"
     if [ -n "$TASK_IP" ] && [ "$TASK_IP" != "null" ]; then
         export EUREKA_INSTANCE_IP_ADDRESS="$TASK_IP"
     fi
