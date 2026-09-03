d075ed0c77769f87928e11530cff9fbb09ba9709
Gina Peng <ginapeng2002@gmail.com>
2026-09-03 13:01:48 -0500

    test

--- auth-service/src/main/java/com/skillstorm/auth_service/Services/AuthService.java
+++ auth-service/src/main/java/com/skillstorm/auth_service/Services/AuthService.java
@@ -27,7 +27,7 @@ import com.skillstorm.auth_service.Entities.User;
 import com.skillstorm.auth_service.Enums.UserRole; 
 import com.skillstorm.auth_service.Exceptions.DuplicateEmailException;
 import com.skillstorm.auth_service.Repositories.UserRepository;
-import com.skillstorm.auth_service.clients.StaffingClient;
+import com.skillstorm.auth_service.clients.StaffingClient; 
 
 @Service
 public class AuthService {
