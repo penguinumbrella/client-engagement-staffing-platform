e97e3fa0abc4f690812af3a54a0304fa1380b88b
Gina Peng <ginapeng2002@gmail.com>
2026-09-04 09:14:10 -0500

    center spinny bee

--- frontend/src/app/shared/spinny-bee/spinny-bee.ts
+++ frontend/src/app/shared/spinny-bee/spinny-bee.ts
@@ -11,17 +11,18 @@ import { Component, input } from '@angular/core';
       aria-label="Loading"
       class="flex w-full items-center justify-center"
       [class.h-full]="size() !== 'sm'"
-      [class.min-h-48]="size() !== 'sm'"
+      [class.min-h-full]="size() !== 'sm'"
+      [class.flex-1]="size() !== 'sm'"
       [class.py-2]="size() === 'sm'"
     >
       <img
         src="/images/auth/spinny-bee.png"
         alt=""
-        class="animate-spin object-contain"
+        class="spinny-bee object-contain"
         [class.h-12]="size() === 'sm'"
         [class.w-12]="size() === 'sm'"
-        [class.h-24]="size() !== 'sm'"
-        [class.w-24]="size() !== 'sm'"
+        [class.h-28]="size() !== 'sm'"
+        [class.w-28]="size() !== 'sm'"
       >
     </div>
   `,
